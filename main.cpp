f87d2870525cbfa6588771c4a2691ce5f710f720
ZhaoXiaoLin01 <43593495+ZhaoXiaoLin01@users.noreply.github.com>
2021-05-17 16:31:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include"calculator.h"
 using namespace std;
  
-int main()
+int  main()
 {
 	Calculator calculator;
 	string str;
 	cout << "请输入一个数学表达式（其中要求符号是英文，且无空白符）：" << endl;
-	cin >> str;
-	double result = calculator.calculate(str);
-	cout << result << endl;
+	while(true)
+	{
+		cin >> str;
+		double result = calculator.calculate(str);
+		cout << result << endl;
+	}
 	return 0;
- 
 }
